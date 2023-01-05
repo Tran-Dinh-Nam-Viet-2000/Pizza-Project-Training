2b3c767094ef4663c50badb59cda72b578fac16f
viettdnde140214 <viettdnde140214@outlook.com>
2023-01-05 11:27:39 +0700

    hello world

---
+++ Pizza_Project_Training/Pizza_Project_Training/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Pizza_Project_Training
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
